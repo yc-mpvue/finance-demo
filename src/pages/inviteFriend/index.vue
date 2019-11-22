4f0b44ee79a8f882c009257377d0d2896fce27ef
翔君 <1733878114@qq.com>
2019-11-22 09:36:56 +0800

    save

--- src/pages/inviteFriend/index.vue
+++ src/pages/inviteFriend/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="inviteFriend">
     <img class="bg"
-      src=""
+      src="/static/images/banner_yqhy.png"
       alt="">
     <div class="main">
+      <div>
 
+      </div>
     </div>
   </div>
 </template>
@@ -15,5 +17,14 @@ export default {
 </script>
 <style lang="scss" scoped>
 .inviteFriend {
+  font-size: 0;
+  .bg {
+    width: 750rpx;
+    height: 300rpx;
+  }
+  .main {
+    background-color: red;
+    min-height: calc(100vh - 300rpx);
+  }
 }
 </style>
